be792c0378170a592f1855cd80a541039c3293fb
jim <jim@jims-MacBook-Air-2.local>
2025-03-18 12:03:47 -0700

    change to two servers in infra/aws_instance

--- infra/aws_instance/main.tf
+++ infra/aws_instance/main.tf
@@ -16,29 +16,51 @@ resource "aws_subnet" "subnet" {
   }
 }
 
-resource "aws_network_interface" "adapter" {
+resource "aws_network_interface" "adapter1" {
   subnet_id   = aws_subnet.subnet.id
   private_ips = ["172.16.10.100"]
 
   tags = {
-    Name = "primary_network_interface"
+    Name = "primary_network_interface1"
   }
 }
 
-resource "aws_instance" "aws_instance" {
+resource "aws_network_interface" "adapter2" {
+  subnet_id   = aws_subnet.subnet.id
+  private_ips = ["172.16.10.101"]
+
+  tags = {
+    Name = "primary_network_interface2"
+  }
+}
+
+resource "aws_instance" "aws_instance1" {
   ami           = "ami-0b6d6dacf350ebc82"
   instance_type = "t2.micro"
 
 
   network_interface {
-    network_interface_id = aws_network_interface.adapter.id
+    network_interface_id = aws_network_interface.adapter1.id
     device_index         = 0
   }
 
   tags = {
-    Name = "AWS_Instance"
+    Name = "AWS_Instance1"
   }
-
 }
 
+resource "aws_instance" "aws_instance2" {
+  ami           = "ami-0b6d6dacf350ebc82"
+  instance_type = "t2.micro"
+
+
+  network_interface {
+    network_interface_id = aws_network_interface.adapter2.id
+    device_index         = 0
+  }
+
+  tags = {
+    Name = "AWS_Instance2"
+  }
+}
 
